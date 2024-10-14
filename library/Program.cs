58a8fda7a743d3c5d50640e07fa17dfc06159c8b
Yamen-Shb <yamen1929@hotmail.com>
2024-10-14 12:32:11 +0300

    Add project files.

---
+++ library/Program.cs
@@ -0,0 +1,19 @@
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(String[] args)
+    {
+        Book book = new Book();
+
+        book.Title = "C# for beginners";
+        book.Author = "Billgates";
+        book.ISBN = "12345678";
+
+        Console.WriteLine($"Book title: {book.Title}");
+        Console.WriteLine($"Book author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+    }
+}
